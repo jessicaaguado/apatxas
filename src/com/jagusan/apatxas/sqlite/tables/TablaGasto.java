8455c73b89eb4a2518c08dd19c405c85d1bf1f29
jessica <jessica.aguado@gmail.com>
2014-10-27 09:56:58 +0100

    cambios en los nombres de columnas de la tabla gasto

--- src/com/jagusan/apatxas/sqlite/tables/TablaGasto.java
+++ src/com/jagusan/apatxas/sqlite/tables/TablaGasto.java
@@ -6,13 +6,13 @@ public class TablaGasto implements BaseColumns {
 
 	public static final String NOMBRE_TABLA = "gasto";
 	public static final String COLUMNA_ID = "id";
-	public static final String COLUMNA_NOMBRE = "nombre";
+	public static final String COLUMNA_CONCEPTO = "concepto";
 	public static final String COLUMNA_TOTAL = "total";
 	public static final String COLUMNA_ID_PAGADO_POR = "idPersona";
 	public static final String COLUMNA_ID_APATXA = "idApatxa";
 
 	public static final String CREATE_TABLE = "CREATE TABLE " + NOMBRE_TABLA + "(" + COLUMNA_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + 
-																					 COLUMNA_NOMBRE + " TEXT," + 
+																					 COLUMNA_CONCEPTO + " TEXT," + 
 																					 COLUMNA_TOTAL + " REAL," + 
 																					 COLUMNA_ID_PAGADO_POR + " INTEGER," +																					  
 																					 COLUMNA_ID_APATXA + " INTEGER" + ")";
