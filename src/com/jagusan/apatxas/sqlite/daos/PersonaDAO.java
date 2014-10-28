59e494173520cc592d8456a66ac0ef6e3ff84997
jessica <jessica.aguado@gmail.com>
2014-10-28 12:03:38 +0100

    corregido nombre de la tabla de las queries

--- src/com/jagusan/apatxas/sqlite/daos/PersonaDAO.java
+++ src/com/jagusan/apatxas/sqlite/daos/PersonaDAO.java
@@ -10,17 +10,18 @@ import android.util.Log;
 
 import com.jagusan.apatxas.sqlite.daos.cursorReader.ExtraerInformacionPersonaDeCursor;
 import com.jagusan.apatxas.sqlite.modelView.PersonaListado;
-import com.jagusan.apatxas.sqlite.tables.TablaApatxa;
 import com.jagusan.apatxas.sqlite.tables.TablaPersona;
 
 public class PersonaDAO {
 
-	private SQLiteDatabase database;
-
+	private static final String NOMBRE_TABLA_PERSONA = TablaPersona.NOMBRE_TABLA;
+	
 	private static final String[] COLUMNAS_PERSONA = { TablaPersona.COLUMNA_ID, TablaPersona.COLUMNA_NOMBRE, TablaPersona.COLUMNA_ID_APATXA, TablaPersona.COLUMNA_CUANTIA_PAGO,
-			TablaPersona.COLUMNA_PAGADO };
-
+		TablaPersona.COLUMNA_PAGADO };
+	
 	private static final String ORDEN_PERSONAS_DEFECTO = TablaPersona.COLUMNA_NOMBRE + " ASC";
+
+	private SQLiteDatabase database;	
 
 	public void setDatabase(SQLiteDatabase database) {
 		this.database = database;
@@ -31,19 +32,19 @@ public class PersonaDAO {
 		values.put(TablaPersona.COLUMNA_NOMBRE, nombre);
 		values.put(TablaPersona.COLUMNA_ID_APATXA, idApatxa);
 
-		long insertId = database.insert(TablaApatxa.NOMBRE_TABLA, null, values);
+		long insertId = database.insert(NOMBRE_TABLA_PERSONA, null, values);
 		Log.d("APATXAS", "DAO: Creada persona con id " + insertId);
 		return insertId;
 	}
 
 	public void borrarPersona(Long id) {
-		database.delete(TablaPersona.NOMBRE_TABLA, TablaApatxa.COLUMNA_ID + " = " + id, null);
+		database.delete(NOMBRE_TABLA_PERSONA, TablaPersona.COLUMNA_ID + " = " + id, null);
 		Log.d("APATXAS", "DAO: Borrada persona con id " + id);
 	}
 
 	public Long recuperarIdPersonaPorNombre(String nombre) {
 		Long idPersona = null;
-		Cursor cursor = database.query(TablaApatxa.NOMBRE_TABLA, new String[] { TablaPersona.COLUMNA_ID }, TablaApatxa.COLUMNA_NOMBRE + "= " + nombre, null, null, null, ORDEN_PERSONAS_DEFECTO);
+		Cursor cursor = database.query(NOMBRE_TABLA_PERSONA, new String[] { TablaPersona.COLUMNA_ID }, TablaPersona.COLUMNA_NOMBRE + "= " + nombre, null, null, null, ORDEN_PERSONAS_DEFECTO);
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
 			idPersona = cursor.getLong(0);
@@ -54,7 +55,7 @@ public class PersonaDAO {
 
 	public List<PersonaListado> recuperarPersonasApatxa(Long idApatxa) {
 		List<PersonaListado> personas = new ArrayList<PersonaListado>();
-		Cursor cursor = database.query(TablaPersona.NOMBRE_TABLA, COLUMNAS_PERSONA, null, null, null, null, ORDEN_PERSONAS_DEFECTO);
+		Cursor cursor = database.query(NOMBRE_TABLA_PERSONA, COLUMNAS_PERSONA, null, null, null, null, ORDEN_PERSONAS_DEFECTO);
 
 		cursor.moveToFirst();
 		while (!cursor.isAfterLast()) {
