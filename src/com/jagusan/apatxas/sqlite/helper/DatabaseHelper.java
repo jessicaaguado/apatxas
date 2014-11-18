ddb979a552d17045b2bda60cafb618d15dd9edcd
jessica <jessica.aguado@gmail.com>
2014-11-18 09:58:25 +0100

    version de base de datos 1

--- src/com/jagusan/apatxas/sqlite/helper/DatabaseHelper.java
+++ src/com/jagusan/apatxas/sqlite/helper/DatabaseHelper.java
@@ -12,7 +12,7 @@ import com.jagusan.apatxas.sqlite.tables.TablaPersona;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
 	private static final String NOMBRE_BASE_DATOS = "apatxas.db";
-	private static final int VERSION_BASE_DATOS = 2;
+	private static final int VERSION_BASE_DATOS = 1;
 
 	public DatabaseHelper(Context context) {
 		super(context, NOMBRE_BASE_DATOS, null, VERSION_BASE_DATOS);
