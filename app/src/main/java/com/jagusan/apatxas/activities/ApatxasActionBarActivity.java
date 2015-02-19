509e6343b3c4a2541fcce64c60e578f0c1e8b6d9
jessica <jessica.aguado@gmail.com>
2015-02-19 15:46:13 +0100

    gestion correcta de los cambios de orientacion para que se  mantenga el idioma y tambien se actualice el actionbar. utilizando android:configChanges el action bar no se redimensionaba correctamente

--- app/src/main/java/com/jagusan/apatxas/activities/ApatxasActionBarActivity.java
+++ app/src/main/java/com/jagusan/apatxas/activities/ApatxasActionBarActivity.java
@@ -1,6 +1,6 @@
 package com.jagusan.apatxas.activities;
 
-import android.content.res.Configuration;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 
@@ -8,10 +8,12 @@ import com.jagusan.apatxas.utils.SettingsUtils;
 
 public class ApatxasActionBarActivity extends ActionBarActivity {
 
+
     @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         SettingsUtils.aplicarSettingsPropios(this);
+
     }
 
     void personalizarActionBar(int idTituloString, boolean mostrar) {
